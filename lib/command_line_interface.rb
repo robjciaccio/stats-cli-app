6132013b11d50c5815d7158eef8928f04cf4491e
github username <github email address>
2017-07-11 15:40:38 -0400

    mapping out all methods to complete functionality

--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -2,3 +2,17 @@ require 'Nokogiri'
 require 'pry'
 require "./lib/listings.rb"
 require "./lib/apartmentsearch.rb"
+
+
+# run method
+
+
+
+#html method
+  #apply zip/rooms/price to html
+
+#selection
+  #return site of selected post
+
+#clear_search
+  #clear all including html
