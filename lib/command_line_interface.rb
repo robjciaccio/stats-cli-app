d7b19c0ff477db5a823423fcd69111322f849405
github username <github email address>
2017-07-12 17:44:22 -0400

    finalized with comments

--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -6,17 +6,19 @@ require "./lib/scraper.rb"
 class CLI
 
   def self.run
-    current = Listing.new('current')
-    current.get_info
+    current = Listing.new('current') #creates new listing 'current'
+    current.get_info #calls Listing class method to get info and set class vatiables
     html = current.get_html("#{current.max_price}", "#{current.zip_code}", "#{current.bedrooms}")
-    Scraper.apartment_scraper("#{html}")
-    choice = current.selection
-    decision = Listing.display_choice(choice)
-    Listing.congrats
-    puts "#{decision}"
+    #above creates html link, by using Listing's get_html class method
+    Scraper.apartment_scraper("#{html}") #begins scraping process using Scraper instance method apartment_scraper
+    choice = current.selection #creates variable of 'listing selection number' using Listings selection class method
+    decision = Listing.display_choice(choice) #turns lising selection variable into an integer using Listing
+    #instance method.
+    Listing.congrats #congratualtes winner
+    puts "#{decision}" #shows selection
   end
 
-  def self.new_search
+  def self.new_search #used for future functionality
     puts "Would you like another search? Y/N:"
     answer = gets.chomp
     if answer == "y" || if answer == "Y"
--- lib/listings.rb
+++ lib/listings.rb
@@ -5,7 +5,7 @@ class Listing
 attr_accessor :max_price, :zip_code, :bedrooms, :new_html
 attr_reader :name #name should not be able to be changed
 
-  def initialize(name) #new listings will be initialized with name
+  def initialize(name) #new listings will be initialized with a name
     @name = name
   end
 
@@ -14,7 +14,7 @@ attr_reader :name #name should not be able to be changed
     @new_html
   end
 
-  def get_info
+  def get_info #save user input to designated instance variables
     puts "Welcome to your apartment search!\n"
     puts "Please enter the zip code you would like to search in: "
     @zip_code = gets.chomp
@@ -24,25 +24,25 @@ attr_reader :name #name should not be able to be changed
     @bedrooms = gets.chomp
   end
 
-  def reset_all
+  def reset_all #class method for future functionality
     @zip_code.clear
     @max_price.clear
     @bedrooms.clear
   end
 
-  def selection
+  def selection # for selecting a specific listing
     puts "Please make a selection: "
     choice = gets.chomp
     choice
   end
 
-  def self.display_choice(num)
+  def self.display_choice(num) #instance method changes selection from string to integer and subtracts 1 for array purposes
     choice = num.to_i
     choice -= 1
     Scraper.all[choice]
   end
 
-  def self.congrats
+  def self.congrats #instance method delivers Congratulatory message to user
     puts "Congratulations!! You have chosen:"
   end
 end
