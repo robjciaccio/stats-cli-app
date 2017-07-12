b8e019eb58ca60f509c58b0a7fd16230392e90ac
github username <github email address>
2017-07-12 12:17:19 -0400

    eliminating more code

--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -3,19 +3,15 @@ require 'pry'
 require "./lib/listings.rb"
 require "./lib/scraper.rb"
 
+
+
+
 class CLI
 
   def self.run
-    puts "Welcome to your apartment search!\n"
-    puts "Please enter the zip code you would like to search in: "
-    zip_code = gets.chomp
-    puts "Please enter your max price range: "
-    max_price = gets.chomp
-    puts "Please enter the minimum amount of bedrooms: "
-    bedrooms = gets.chomp
-    current = Listing.new("#{max_price}", "#{zip_code}", "#{bedrooms}")
-    binding.pry
-    html = current.get_html
+    current = Listing.new('current')
+    current.get_info
+    html = current.get_html("#{current.max_price}", "#{current.zip_code}", "#{current.bedrooms}")
     Scraper.apartment_scraper("#{html}")
   end
 
--- lib/listings.rb
+++ lib/listings.rb
@@ -1,20 +1,20 @@
 
 require "pry"
-class Listing
-attr_accessor :max_price, :zip_code, :bedrooms, :new_html
 
-  def initialize(max_price = "2500", zip_code = "11215", bedrooms = "1")
-    @max_price = max_price
-    @zip_code = zip_code
-    @bedrooms = bedrooms
+
+class Listing
+attr_accessor :max_price, :zip_code, :bedrooms, :new_html, :name
+
+  def initialize(name)
+    @name = name
   end
 
-  def self.get_html(max_price, zip_code, bedrooms)
+  def get_html(max_price, zip_code, bedrooms)
     @new_html = "https://newyork.craigslist.org/search/aap?postal=" + "#{zip_code}" + "&max_price=" + "#{max_price}" + "&min_bedrooms=" + "#{bedrooms}" + "&availabilityMode=0"
     @new_html
   end
 
-  def self.get_info
+  def get_info
     puts "Welcome to your apartment search!\n"
     puts "Please enter the zip code you would like to search in: "
     @zip_code = gets.chomp
